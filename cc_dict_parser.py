92e64227dbc97d54a13414aae2af0b77f7672303
Felipe Peter <mr-peipei@web.de>
2019-12-06 18:09:34 +0800

    Read in cc dictionary

---
+++ cc_dict_parser.py
@@ -0,0 +1,36 @@
+import re
+
+data = dict()
+
+id = 1
+
+with open('cedict_ts.u8') as f:
+    i_line = 0
+    for line in f:
+        i_line += 1
+        if line[0] != '#':
+            line = line.rstrip()
+
+            headwords = re.split('\s',line)
+            if headwords:
+                data[id] = dict()
+                data[id]['simplified'] = headwords[1]
+                data[id]['traditional'] = headwords[0]
+            else:
+                raise Exception("No headwords found in line {}".format(i_line))
+
+            pinyin = re.search(r"\[(.*)\]" ,line).group(1)
+            if pinyin:
+                data[id]['pinyin'] = pinyin
+            else:
+                raise Exception("No Pinyin for {} in line {}".format(simplified, i_line))
+
+            definition = re.split('\/',line)
+            if definition:
+                data[id]['definition'] = definition[1:-1]
+            else:
+                raise Exception("No definition for {} in line {}".format(simplified, i_line))
+            
+            id += 1
+
+print("Found {} entries".format(id-1))
