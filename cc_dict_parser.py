336e70920c9a544fedcf1a732366197cab79c055
Felipe Peter <mr-peipei@web.de>
2019-12-14 19:32:59 +0800

    Put everything in one function

--- cc_dict_parser.py
+++ cc_dict_parser.py
@@ -22,7 +22,7 @@ def create_dictionary_database(textfile_path, database_path):
             c = conn.cursor()
 
             if not update_db:
-                c.execute('''CREATE TABLE word (
+                c.execute('''CREATE TABLE entries (
                             id INTEGER PRIMARY KEY AUTOINCREMENT, 
                             simplified TEXT NOT NULL,
                             traditional TEXT NOT NULL,
@@ -31,16 +31,29 @@ def create_dictionary_database(textfile_path, database_path):
                             word_length INTEGER NOT NULL)''')
 
                 # Definitions have to be mapped to a headword and a specific pinyin
-                c.execute('''CREATE TABLE definition (
+                c.execute('''CREATE TABLE definitions (
                             id INTEGER PRIMARY KEY,
-                            word_id INTEGER NOT NULL, 
+                            entry_id INTEGER NOT NULL, 
                             definition TEXT NOT NULL)''')
+                
+                # Permutations linked to dictionary entry
+                c.execute('''CREATE TABLE search_index (
+                            id INTEGER PRIMARY KEY,
+                            entry_id INTEGER NOT NULL, 
+                            permutation TEXT NOT NULL)''')
+
+            else:
+                c.execute("DELETE FROM search_index")
+
+            n_lines = sum([1 for line in f if line[0] != '#'])
+            f.seek(0)
 
             for i_line, line in enumerate(f):
                 # Check for comment line
                 if line[0] != '#':
-                    if (i_line % 100) == 0:
-                        print(i_line)
+                    if ((i_line+1) % 1000) == 0:
+                        print("Entry {}/{}".format(i_line+1, n_lines), end='\r')
+                        
                     line = line.rstrip()
 
                     headwords = re.split(r'\s',line)
@@ -52,40 +65,71 @@ def create_dictionary_database(textfile_path, database_path):
                     if not definitions: raise Exception("No definitions in line {}".format(i_line+1))
 
                     # Check if entry exists already
-                    word_id = None
+                    entry_id = None
                     if update_db:
-                        word_id = c.execute("""SELECT 
+                        entry_id = c.execute("""SELECT 
                                                     id 
                                                    FROM 
-                                                    word
+                                                    entries
                                                    WHERE 
                                                     simplified = ?
                                                    AND
                                                     pinyin = ?""", 
                                                 (headwords[1], pinyin)).fetchall()
-                        if word_id:
-                            word_id = word_id[0][0]
+                        if entry_id:
+                            entry_id = entry_id[0][0]
                     
-                    if not word_id:
-                        c.execute("""INSERT INTO word (simplified, traditional, pinyin, priority, word_length) 
+                    if not entry_id:
+                        c.execute("""INSERT INTO entries (simplified, traditional, pinyin, priority, word_length) 
                                      VALUES (?, ?, ?, ?, ?)""",
                                     (headwords[1], headwords[0], pinyin, 0, len(headwords[0])))
 
-                        word_id = c.lastrowid 
+                        entry_id = c.lastrowid 
                     
                     if update_db:
                         # Delete existing definitions
                         c.execute("""DELETE FROM 
-                                     definition
+                                     definitions
                                     WHERE
-                                     word_id = ?""",
-                                    (word_id,))
+                                     entry_id = ?""",
+                                    (entry_id,))
 
                     # Save Definitions
                     for definition in definitions[1:-1]:
-                        c.execute("""INSERT INTO definition (word_id, definition) 
+                        c.execute("""INSERT INTO definitions (entry_id, definition) 
                                      VALUES (?, ?)""",
-                                     (word_id, definition))
+                                     (entry_id, definition))
+
+                    # Generate search index
+                    max_len_permutations = 3
+                    syllables = re.split(r'\s', pinyin)
+                    hanzi = headwords[1]
+
+                    if len(syllables) <= max_len_permutations:
+                        if len(hanzi) != len(syllables):
+                            permutations = create_permutations(['' for x in syllables], syllables)[1:]
+                            permutations.append(hanzi)
+                        else:
+                            permutations = create_permutations(list(hanzi), syllables)
+                    else:
+                        if len(hanzi) != len(syllables):
+                            tmp_permutations = create_permutations(['' for x in syllables[:max_len_permutations]], syllables[:max_len_permutations])[1:]
+                        else:
+                            tmp_permutations = create_permutations(hanzi[:max_len_permutations], syllables[:max_len_permutations])
+
+                        permutations = []
+                        for permutation in tmp_permutations:
+                            permutations.append(permutation + re.sub('\d', '', ''.join(syllables[max_len_permutations:])))
+                            permutations.append(permutation + hanzi[max_len_permutations:])
+                        
+                        if hanzi not in permutations:
+                            permutations.append(hanzi)
+
+
+                    for permutation in permutations:
+                        c.execute("""INSERT INTO search_index (entry_id, permutation) 
+                                    VALUES (?, ?)""", 
+                                    (entry_id, permutation))
 
             conn.commit()
             conn.close()
@@ -96,76 +140,6 @@ def create_dictionary_database(textfile_path, database_path):
             if (conn):
                 conn.close()
                 print("The SQLite connection is closed")
-
-def create_search_index(database_path):
-
-    # TODO: Pinyin with and without tones
-    # TODO: Mix of Pinyin and Hanzi
-
-    try:
-        conn = sqlite3.connect(database_path)
-        print("Successfully connected to database.")
-
-        c = conn.cursor()
-        
-        c.execute("DROP TABLE IF EXISTS search_index")
-
-        # Permutations linked to dictionary entry
-        c.execute('''CREATE TABLE search_index
-                    (id INTEGER PRIMARY KEY,
-                    word_id INTEGER NOT NULL,
-                    permutation TEXT NOT NULL)''')
-
-        entries = c.execute("""SELECT 
-                                id, simplified, pinyin
-                             FROM
-                                word""").fetchall()
-        
-        for i, entry in enumerate(entries):
-            if (i%10000)==0:
-                print(i)
-
-            entry_id = entry[0]
-            hanzi = entry[1]
-            syllables = re.split(r'\s', entry[2])
-
-            max_len_permutations = 3
-            
-            if len(syllables) <= max_len_permutations:
-                if len(hanzi) != len(syllables):
-                    permutations = create_permutations(['' for x in syllables], syllables)[1:]
-                    permutations.append(hanzi)
-                else:
-                    permutations = create_permutations(list(hanzi), syllables)
-            else:
-                if len(hanzi) != len(syllables):
-                    tmp_permutations = create_permutations(['' for x in syllables[:max_len_permutations]], syllables[:max_len_permutations])[1:]
-                else:
-                    tmp_permutations = create_permutations(hanzi[:max_len_permutations], syllables[:max_len_permutations])
-
-                permutations = []
-                for permutation in tmp_permutations:
-                    permutations.append(permutation + re.sub('\d', '', ''.join(syllables[max_len_permutations:])))
-                    permutations.append(permutation + hanzi[max_len_permutations:])
-                
-                if hanzi not in permutations:
-                    permutations.append(hanzi)
-
-
-            for permutation in permutations:
-                c.execute("""INSERT INTO search_index (word_id, permutation) 
-                             VALUES (?, ?)""", 
-                             (entry_id, permutation,))
-
-        conn.commit()
-        conn.close()
-
-    except sqlite3.Error as error:
-        print("Failed to insert data into sqlite table", error)
-    finally:
-        if (conn):
-            conn.close()
-            print("The SQLite connection is closed")
 
 
 def create_permutations(hanzi, syllables):
@@ -197,9 +171,9 @@ def create_permutations(hanzi, syllables):
 
         return output
 
+
 if __name__ == "__main__":
     textfile_path = 'cedict_ts.u8'
     database_path = 'cedict.db'
     
     create_dictionary_database(textfile_path, database_path)
-    create_search_index(database_path)
