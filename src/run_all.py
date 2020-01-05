701b4da592537bd761bbfcdea816441e56a527ee
Felipe Peter <mr-peipei@web.de>
2020-01-05 13:01:59 +0800

    Include stroke orders into database

--- src/run_all.py
+++ src/run_all.py
@@ -1,10 +1,14 @@
 from cedict_parser import parse_dictionary
 from cjdecomp_parser import parse_decompositions
+from stroke_order_parser import parse_stroke_order
 
 cedict_raw_file_path = "./data/cedict.txt"
 cjdecomp_raw_file_path = "./data/cjdecomp.txt"
+stroke_order_raw_file_path = "./data/stroke_order.json"
 database_path = "./output/data.db"
 
 parse_dictionary(cedict_raw_file_path, database_path, max_len_permutations=3)
 
 parse_decompositions(cjdecomp_raw_file_path, database_path)
+
+parse_stroke_order(stroke_order_raw_file_path, database_path)
---
+++ src/stroke_order_parser.py
@@ -0,0 +1,53 @@
+import json
+import os
+import sqlite3
+
+
+def parse_stroke_order(textfile_path, database_path):
+
+    if not os.path.isfile(textfile_path):
+        raise Exception("Couldn't find {}!".format(textfile_path))
+
+    with open(textfile_path) as f:
+        print("Opened stroke order text file.")
+
+        data = json.load(f)
+
+        try:
+            conn = sqlite3.connect(database_path)
+            print("Successfully connected to database.")
+
+            c = conn.cursor()
+
+            c.execute("DROP TABLE IF EXISTS stroke_orders")
+
+            c.execute('''CREATE TABLE stroke_orders (
+                        id INTEGER PRIMARY KEY,
+                        character TEXT NOT NULL,
+                        json TEXT NOT NULL
+                        );''')
+
+            for character in data:
+                c.execute("""INSERT INTO stroke_orders (character, json) 
+                                VALUES (?, ?)""",
+                                (character, str(data[character])))
+
+            conn.commit()
+            conn.close()
+
+            print("Successfully parsed stroke orders.")
+
+        except sqlite3.Error as error:
+            print("Failed to insert data into sqlite table:", error)
+        finally:
+            if (conn):
+                conn.close()
+                print("The SQLite connection is closed.")
+
+
+
+if __name__ == "__main__":
+    stroke_order_raw_file_path = "./data/stroke_order.json"
+    database_path = "./output/data.db"
+
+    parse_stroke_order(stroke_order_raw_file_path, database_path)
