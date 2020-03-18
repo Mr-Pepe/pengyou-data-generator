8add64f1295f7f24b0b6c9f318893fb01d6caa71
Felipe Peter <mr-peipei@web.de>
2020-03-18 20:27:21 +0800

    Only save lower case of permutations

--- src/permutations.py
+++ src/permutations.py
@@ -3,7 +3,6 @@ import re
 
 
 def generate_permutations(database_path, max_len_permutations):
-
     try:
         conn = sqlite3.connect(database_path)
 
@@ -12,7 +11,7 @@ def generate_permutations(database_path, max_len_permutations):
         c.execute("DROP INDEX IF EXISTS search_index")
         c.execute("DROP TABLE IF EXISTS permutations")
         c.execute("VACUUM")
-        
+
         # Permutations of Hanzi and Pinyin with and without tones for searching
         c.execute('''CREATE TABLE permutations (
                     id INTEGER PRIMARY KEY,
@@ -24,10 +23,10 @@ def generate_permutations(database_path, max_len_permutations):
         print("Generating permutations.")
 
         for entry in entries:
-            
+
             entry_id = entry[0]
             hanzi = entry[1]
-            syllables = re.split(r'\s', entry[3].lower())
+            syllables = re.split(r'\s', entry[3])
 
             if len(syllables) <= max_len_permutations:
                 if len(hanzi) != len(syllables):
@@ -37,24 +36,24 @@ def generate_permutations(database_path, max_len_permutations):
                     permutations = create_permutations(list(hanzi), syllables)
             else:
                 if len(hanzi) != len(syllables):
-                    tmp_permutations = create_permutations(['' for x in syllables[:max_len_permutations]], syllables[:max_len_permutations])[1:]
+                    tmp_permutations = create_permutations(['' for x in syllables[:max_len_permutations]],
+                                                           syllables[:max_len_permutations])[1:]
                 else:
-                    tmp_permutations = create_permutations(hanzi[:max_len_permutations], syllables[:max_len_permutations])
+                    tmp_permutations = create_permutations(hanzi[:max_len_permutations],
+                                                           syllables[:max_len_permutations])
 
                 permutations = []
                 for permutation in tmp_permutations:
                     permutations.append(permutation + re.sub(r'\d', '', ''.join(syllables[max_len_permutations:])))
                     permutations.append(permutation + hanzi[max_len_permutations:])
-                
+
                 if hanzi not in permutations:
                     permutations.append(hanzi)
 
-
             for permutation in permutations:
                 c.execute("""INSERT INTO permutations (entry_id, permutation) 
-                            VALUES (?, ?)""", 
-                            (entry_id, permutation))
-
+                            VALUES (?, ?)""",
+                          (entry_id, permutation))
 
         print("Creating search index for permutations.")
         c.execute("CREATE INDEX search_index ON permutations (permutation)")
@@ -73,9 +72,13 @@ def generate_permutations(database_path, max_len_permutations):
 
 def create_permutations(hanzi, syllables):
     if len(syllables) == 1:
-        syllable = syllables[0]
+        syllable = syllables[0].lower()
+        hanzi = hanzi[0].lower()
 
-        output = [hanzi[0], syllable]
+        if syllable == hanzi:
+            output = [syllable]
+        else:
+            output = [hanzi, syllable]
 
         if bool(re.search(r'\d', syllable)):
             syllable_without_digits = re.sub(r'\d', '', syllable)
@@ -84,11 +87,10 @@ def create_permutations(hanzi, syllables):
         if syllable == 'r5':
             output.append('er5')
             output.append('er')
-        
-        
+
         return list(set(output))
     else:
-        split = int(len(syllables)/2)
+        split = int(len(syllables) / 2)
         first_half = create_permutations(hanzi[:split], syllables[:split])
         second_half = create_permutations(hanzi[split:], syllables[split:])
 
@@ -99,8 +101,6 @@ def create_permutations(hanzi, syllables):
                 output.append(first_option + second_option)
 
         return output
-
-
 
 
 if __name__ == "__main__":
